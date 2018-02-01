c899ad356a6674c748a49ee3db916b976134381c
Jonek2208 <jonatanhrynko@gmail.com>
2018-02-02 00:38:29 +0100

    Commit

--- WROI2018/kan0/kan.cpp
+++ WROI2018/kan0/kan.cpp
@@ -6,6 +6,7 @@ typedef long long LL;
 typedef vector<int> VI;
 
 const int MAXM = 1e5+2;
+const int MAXN = 1e7+2;
 
 #define FOR(i, b, e) for(int i = b; i <= e; ++i)
 #define FORD(i, b, e) for(int i = b; i >= e; --i)
@@ -19,21 +20,32 @@ const int MAXM = 1e5+2;
 
 #define DEBUG(s) s
 
-int per[MAXM], tmp[MAXM], stos[MAXM], kon[MAXM];
+int tab[MAXM], per[MAXM], stos[MAXM], kon[MAXM];
+int cnt;
 bool odw[MAXM];
 vector<VI> cykle;
-int n, m, q, a;
+int n, m, q, a, t, s;
+
+int query(int x)
+{
+	x =  n-x+1;
+	int res = 0;
+	return res;
+}
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin>>n>>m>>q;
 	
-	FOR(i, 1, m) cin>>per[i];
-	FOR(i, 1, n) stos[i] = i;
+	FOR(i, 1, m) cin>>tab[i];
 	
+	FOR(i, 1, m) per[i] = (tab[i] - 1 + m)%(m+1)+1;
+	per[m+1] = m;
 	
-	FOR(i, 1, m)
+	DEBUG(FOR(i, 1, m+1) cout<<per[i]<<" "; cout<<endl;)
+	
+	FORD(i, m+1, 1)
 	{
 		if(!odw[i])
 		{
@@ -57,11 +69,30 @@ int main()
 	)
 	
 	/*tasowanie*/
+	t = n-m+1;
+	s = SIZE(cykle[0]);
+	
+	FOR(i, 1, s-2)
+	{
+		stos[cykle[0][i]] = n-i+1;
+	}
+	
+	FOR(i, 1, SIZE(cykle)-1)
+	{
+		REP(j, SIZE(cykle[i]))
+		{
+			stos[(cykle[i][j] + t - 1)%s+1] = cykle[i][j];
+		}
+	}
+	
+	FOR(i, 1, n) kon[n-i+1] = stos[i];
+	
+	DEBUG(FOR(i, 1, n) cout<<stos[i]<<" "; cout<<endl;)
 
 	while(q--)
 	{
 		cin>>a;
-		cout<<kon[a]<<"\n";
+		cout<<query(a)<<"\n";
 	}
 	
 	
@@ -73,17 +104,14 @@ int main()
 
 5 3 5
 3
-15 3 0
-3
 1
-2
-
 2
 1
 2
 3
 4
 5
+
 
 5 3 0
 3
@@ -98,11 +126,18 @@ int main()
 1 
 2
 
-10 5 0
+20 5 0
 4 
 5 
 1 
 2 
 3
+
+12 4 0
+4
+1
+2
+3
+
 
 */
